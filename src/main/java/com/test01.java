032668802fcad66743a3f91b6ea4c6743e0771c7
zhaobufan <13415489@qq.com>
2022-01-19 22:54:06 +0800

    修改数据

---
+++ src/main/java/com/test01.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @Author: zhaobf
+ * @Date: 2022/1/19-01-19-下午10:53
+ * @Description:
+ * @version: 1.0
+ **/
+public class test01 {
+
+    public static void main(String[] args) {
+        System.out.println(222);
+    }
+}
