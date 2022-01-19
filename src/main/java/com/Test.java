358f8b8595f7bba2d6ba8fa6ee06b9cedb21a54d
zhaobufan <13415489@qq.com>
2022-01-19 22:43:44 +0800

    第一次进行数据的提交

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @Author: zhaobf
+ * @Date: 2022/1/19-01-19-下午10:38
+ * @Description:
+ * @version: 1.0
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
